3498614c3be32e313e6cce0b3729e116750739ac
Aatir Manzur <aatrapps@gmail.com>
2023-08-21 15:29:32 -0400

    fix conditions

--- median.py
+++ median.py
@@ -1,18 +1,20 @@
+from typing import List
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         len_nums1 = len(nums1)
         len_nums2 = len(nums2)
-        merged[]
+        merged = []
         i = 0
         j = 0
-        while (i< len(nums1) || j < len(nums2)):
+        while (i != len(nums1) or j != len(nums2)):
             if nums1[i] < nums2[j]:
                 merged.append(nums1[i])
                 if (i + 1 < len(nums1)):
 
 
                     i = i + 1 
-            elif nums1[i] > nums2[j]):
+            elif nums1[i] > nums2[j]:
                 merged.append(nums2[j])
                 if (j + 1 < len(nums2)):
                     j = j + 1
@@ -23,3 +25,7 @@ class Solution:
                 if (j + 1 < len(nums2)):
                     j = j + 1
         return merged[(len(merged)-1)//2]
+if __name__ == "__main__":
+
+    a = Solution()
+    print( a.findMedianSortedArrays([1,2,3,4], [5,6,7]))
