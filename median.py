e4accf6b1cf76d58549efd614e7b157555881f04
Aatir Manzur <aatrapps@gmail.com>
2023-08-21 09:37:59 -0400

    median

---
+++ median.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len_nums1 = len(nums1)
+        len_nums2 = len(nums2)
+        merged[]
+        j = 0
+        for i,element in enumerate(nums1):
+            if element < nums2[i]:
+                merged.append(element)
+            else:
+                merged.append(nums2[i])
+            j++
+        if j < len_nums2-1:
+            merged.append(nums2[j:])
+        return merged[(len(merged)-1)//2]
