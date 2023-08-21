dde2607fc69d60e2cf3731675a7f58d5bfdc66fc
Aatir Manzur <aatrapps@gmail.com>
2023-08-21 10:11:47 -0400

    use the right index

--- median.py
+++ median.py
@@ -5,11 +5,12 @@ class Solution:
         merged[]
         j = 0
         for i,element in enumerate(nums1):
-            if element < nums2[i]:
+
+            if element < nums2[j]:
                 merged.append(element)
             else:
-                merged.append(nums2[i])
-            j++
+                merged.append(nums2[j])
+                j++
         if j < len_nums2-1:
             merged.extend(nums2[j:])
         return merged[(len(merged)-1)//2]
