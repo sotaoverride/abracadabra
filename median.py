a6f20b1607e96251470c7f2017cffd495fc7d73d
Aatir Manzur <aatrapps@gmail.com>
2023-08-25 15:27:57 -0400

    optimize

--- median.py
+++ median.py
@@ -8,16 +8,18 @@ class Solution:
         i = 0
         j = 0
         while ( i < len(nums1) and j < len(nums2) ):
-            if nums1[i] <= nums2[j]:
-                merged.append(nums1[i])
+            x = nums1[i]
+            y = nums2[j]
+            if x <= y:
+                merged.append(x)
                 i = i + 1
-            if nums1[i] >= nums2[j]:
-                merged.append(nums2[j])
+            if y <= x:
+                merged.append(y)
                 j = j + 1
         merged.extend(nums1[i:])
         merged.extend(nums2[j:])
         if (len(merged) & 1) and len(merged) >= 1:
-            return (merged[len(merged)//2]+merged[len(merged)//2-1])/2
+            return merged[(len(merged)-1)//2]
         elif len(merged) > 2:
             return (merged[len(merged)//2]+merged[len(merged)//2-1])/2
 if __name__ == "__main__":
